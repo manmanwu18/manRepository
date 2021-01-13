8f8431aafe8a90b793e15e84590c80b30cbfaf86
wumanman1118@163.com <wumanman1118@163.com>
2021-01-13 14:13:11 +0800

    提交说明

--- src/test/java/com/my/jenkins/demo/Test.java
+++ src/test/java/com/my/jenkins/demo/Test.java
@@ -1,8 +1,18 @@
 package com.my.jenkins.demo;
 
+import org.testng.Assert;
+
 /**
  * mmw
  * 2021/1/13
  */
 public class Test {
+    @org.testng.annotations.Test
+    public void case1(){
+        Assert.assertEquals(1,1);
+    }
+@org.testng.annotations.Test
+    public void case2(){
+        Assert.assertEquals("hello","hello");
+    }
 }
