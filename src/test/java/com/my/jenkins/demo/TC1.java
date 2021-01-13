282f4d023d87f99e405697940940b89e6ac1f4e4
wumanman1118@163.com <wumanman1118@163.com>
2021-01-13 13:56:13 +0800

    add file to test folder

---
+++ src/test/java/com/my/jenkins/demo/TC1.java
@@ -0,0 +1,19 @@
+package com.my.jenkins.demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * mmw
+ * 2021/1/13
+ */
+public class TC1 {
+    @Test
+    public void case1(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void case2(){
+        Assert.assertEquals("hello","hello");
+    }
+}
